50467a00293198fcf706c4c6264d79ee32eb8d8e
bes01 <bkapa18@freeuni.edu.ge>
2022-04-18 20:44:09 +0400

    initial project

---
+++ src/main/java/ge/edu/freeuni/petcarebackend/PetCareBackEndApplication.java
@@ -0,0 +1,13 @@
+package ge.edu.freeuni.petcarebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetCareBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetCareBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ge/edu/freeuni/petcarebackend/PetCareBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package ge.edu.freeuni.petcarebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetCareBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
