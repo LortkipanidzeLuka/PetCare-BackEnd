7d535d723e19e4488c20b956b76af922deb9f5ae
bes01 <bkapa18@freeuni.edu.ge>
2022-06-05 17:01:03 +0400

    added crud tests to lost found controller

--- src/main/java/ge/edu/freeuni/petcarebackend/controller/dto/AdvertisementImageDTO.java
+++ src/main/java/ge/edu/freeuni/petcarebackend/controller/dto/AdvertisementImageDTO.java
@@ -1,6 +1,7 @@
 package ge.edu.freeuni.petcarebackend.controller.dto;
 
 import ge.edu.freeuni.petcarebackend.repository.entity.AdvertisementImageEntity;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,6 +9,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class AdvertisementImageDTO {
 
     private String title;
