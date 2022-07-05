76173b6b78a45bf2d483a7f455bc0730a7c1a322
bes01 <bkapa18@freeuni.edu.ge>
2022-07-05 18:07:55 +0400

    soft delete job added

--- src/main/java/ge/edu/freeuni/petcarebackend/jobs/ExpiredAdvertisementJob.java
+++ src/main/java/ge/edu/freeuni/petcarebackend/jobs/ExpiredAdvertisementJob.java
@@ -1,23 +1,29 @@
 package ge.edu.freeuni.petcarebackend.jobs;
 
 import ge.edu.freeuni.petcarebackend.repository.AdvertisementRepository;
+import ge.edu.freeuni.petcarebackend.repository.entity.AdvertisementEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Service
 @Transactional
 public class ExpiredAdvertisementJob {
 
-    private final AdvertisementRepository advertisementRepository;
+    private final AdvertisementRepository repository;
 
-    public ExpiredAdvertisementJob(AdvertisementRepository advertisementRepository) {
-        this.advertisementRepository = advertisementRepository;
+    public ExpiredAdvertisementJob(AdvertisementRepository repository) {
+        this.repository = repository;
     }
 
     @Scheduled(cron = "0 0 * * *")
     public void softDeleteExpiredAdvertisements() {
-//        TODO: soft delete advertisements
+        LocalDate lastValidDate = LocalDate.now().minusDays(30);
+        List<AdvertisementEntity> expiredAdvertisements = repository.findByCreateDateBeforeAndExpired(lastValidDate, false);
+        expiredAdvertisements.forEach(adv -> adv.setExpired(true));
     }
 
 }
