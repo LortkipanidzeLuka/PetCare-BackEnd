114570e7a6aeb214c4f6379637ac7a703b8fa51b
bes01 <bkapa18@freeuni.edu.ge>
2022-07-07 22:56:32 +0400

    added image validation

--- src/main/java/ge/edu/freeuni/petcarebackend/controller/dto/AdvertisementImageDTO.java
+++ src/main/java/ge/edu/freeuni/petcarebackend/controller/dto/AdvertisementImageDTO.java
@@ -1,10 +1,16 @@
 package ge.edu.freeuni.petcarebackend.controller.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import ge.edu.freeuni.petcarebackend.repository.entity.AdvertisementImageEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.AssertTrue;
+import java.util.Base64;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Data
 @Builder
@@ -22,5 +28,24 @@ public class AdvertisementImageDTO {
         this.title = advertisementImage.getTitle();
         this.content = advertisementImage.getContent();
         this.isPrimary = advertisementImage.getIsPrimary();
+    }
+
+    @JsonIgnore
+    @AssertTrue
+    public boolean isValid() {
+        if (content == null) {
+            return false;
+        }
+        Matcher matcher = Pattern.compile("^data:image/(jpg|jpeg|png);base64,").matcher(content);
+        if (!matcher.matches()) {
+            return false;
+        }
+        byte[] decodedImageBytes;
+        try {
+            decodedImageBytes = Base64.getMimeDecoder().decode(content.substring(content.indexOf(',')));
+        } catch (Exception e) {
+            return false;
+        }
+        return decodedImageBytes.length < 5 * 1024 * 1024;
     }
 }
